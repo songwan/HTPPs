62d6dd04b5dbe0d8250e6d3ce83afd6cb7bf660c
songwan <songwan.joun@gmail.com>
2022-01-06 04:27:53 +0900

    initial commit

---
+++ utils/functions.py
@@ -0,0 +1,261 @@
+from pathlib import Path
+import base64
+import time
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.datasets import make_moons, make_circles, make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+
+from models.utils import model_infos, model_urls
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+@st.cache(
+    suppress_st_warning=True,
+    allow_output_mutation=True,
+    show_spinner=True)
+def generate_data(dataset, n_samples, train_noise, test_noise, n_classes, uploaded_file=None):
+    if dataset == "moons":
+        x_train, y_train = make_moons(n_samples=n_samples, noise=train_noise)
+        x_test, y_test = make_moons(n_samples=n_samples, noise=test_noise)
+    elif dataset == "circles":
+        x_train, y_train = make_circles(n_samples=n_samples, noise=train_noise)
+        x_test, y_test = make_circles(n_samples=n_samples, noise=test_noise)
+    elif dataset == "blobs":
+        x_train, y_train = make_blobs(
+            n_features=2,
+            n_samples=n_samples,
+            centers=n_classes,
+            cluster_std=train_noise * 47 + 0.57,
+            random_state=42,
+        )
+        x_test, y_test = make_blobs(
+            n_features=2,
+            n_samples=n_samples // 2,
+            centers=2,
+            cluster_std=test_noise * 47 + 0.57,
+            random_state=42,
+        )
+
+        scaler = StandardScaler()
+        x_train = scaler.fit_transform(x_train)
+
+        x_test = scaler.transform(x_test)
+    elif dataset == "upload":
+        current_data = pd.read_csv(uploaded_file)
+        # n_samples = current_data.shape[0]
+        # cur_data = st.session_state.cur_data
+        # x = cur_data.iloc[:, 0]
+        # y = cur_data.iloc[:, 1:]
+        # x_train, x_test, y_train, y_test = train_test_split(x, y,
+        #                                                    test_size=0.2, random_state=1234)
+
+    return x_train, y_train, x_test, y_test
+
+
+# Plotting decision boundary is relevant for classification tasks
+
+def plot_decision_boundary_and_metrics(
+        model, x_train, y_train, x_test, y_test, metrics
+):
+    d = x_train.shape[1]
+
+    x_min, x_max = x_train[:, 0].min() - 1, x_train[:, 0].max() + 1
+    y_min, y_max = x_train[:, 1].min() - 1, x_train[:, 1].max() + 1
+
+    h = 0.02
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    y_ = np.arange(y_min, y_max, h)
+
+    model_input = [(xx.ravel() ** p, yy.ravel() ** p) for p in range(1, d // 2 + 1)]
+    aux = []
+    for c in model_input:
+        aux.append(c[0])
+        aux.append(c[1])
+
+    Z = model.predict(np.concatenate([v.reshape(-1, 1) for v in aux], axis=1))
+
+    Z = Z.reshape(xx.shape)
+
+    fig = make_subplots(
+        rows=2,
+        cols=2,
+        specs=[[{"colspan": 2}, None], [{"type": "indicator"}, {"type": "indicator"}]],
+        subplot_titles=("Decision Boundary", None, None),
+        row_heights=[0.7, 0.30],
+    )
+
+    heatmap = go.Heatmap(
+        x=xx[0],
+        y=y_,
+        z=Z,
+        colorscale=["tomato", "rgb(27,158,119)"],
+        showscale=False,
+    )
+
+    train_data = go.Scatter(
+        x=x_train[:, 0],
+        y=x_train[:, 1],
+        name="train data",
+        mode="markers",
+        showlegend=True,
+        marker=dict(
+            size=10,
+            color=y_train,
+            colorscale=["tomato", "green"],
+            line=dict(color="black", width=2),
+        ),
+    )
+
+    test_data = go.Scatter(
+        x=x_test[:, 0],
+        y=x_test[:, 1],
+        name="test data",
+        mode="markers",
+        showlegend=True,
+        marker_symbol="cross",
+        visible="legendonly",
+        marker=dict(
+            size=10,
+            color=y_test,
+            colorscale=["tomato", "green"],
+            line=dict(color="black", width=2),
+        ),
+    )
+
+    fig.add_trace(heatmap, row=1, col=1, ).add_trace(train_data).add_trace(
+        test_data
+    ).update_xaxes(range=[x_min, x_max], title="x1").update_yaxes(
+        range=[y_min, y_max], title="x2"
+    )
+
+    fig.add_trace(
+        go.Indicator(
+            mode="gauge+number+delta",
+            value=metrics["test_accuracy"],
+            title={"text": f"Accuracy (test)"},
+            domain={"x": [0, 1], "y": [0, 1]},
+            gauge={"axis": {"range": [0, 1]}},
+            delta={"reference": metrics["train_accuracy"]},
+        ),
+        row=2,
+        col=1,
+    )
+
+    fig.add_trace(
+        go.Indicator(
+            mode="gauge+number+delta",
+            value=metrics["test_f1"],
+            title={"text": f"F1 score (test)"},
+            domain={"x": [0, 1], "y": [0, 1]},
+            gauge={"axis": {"range": [0, 1]}},
+            delta={"reference": metrics["train_f1"]},
+        ),
+        row=2,
+        col=2,
+    )
+
+    fig.update_layout(
+        height=700,
+    )
+
+    return fig
+
+
+def train_classification_model(model, x_train, y_train, x_test, y_test):
+    t0 = time.time()
+    model.fit(x_train, y_train)
+    duration = time.time() - t0
+    y_train_pred = model.predict(x_train)
+    y_test_pred = model.predict(x_test)
+
+    train_accuracy = np.round(accuracy_score(y_train, y_train_pred), 3)
+    train_f1 = np.round(f1_score(y_train, y_train_pred, average="weighted"), 3)
+
+    test_accuracy = np.round(accuracy_score(y_test, y_test_pred), 3)
+    test_f1 = np.round(f1_score(y_test, y_test_pred, average="weighted"), 3)
+
+    return model, train_accuracy, train_f1, test_accuracy, test_f1, duration
+
+
+def train_regression_model(model, x_train, y_train, x_test, y_test):
+    t0 = time.time()
+
+    model.fit(x_train, y_train)
+    duration = time.time() - t0
+    y_train_pred = model.predict(x_train)
+    y_test_pred = model.predict(x_test)
+
+    train_accuracy = np.round(accuracy_score(y_train, y_train_pred), 3)
+    # train_f1 = np.round(f1_score(y_train, y_train_pred, average="weighted"), 3)
+
+    test_accuracy = np.round(accuracy_score(y_test, y_test_pred), 3)
+    # test_f1 = np.round(f1_score(y_test, y_test_pred, average="weighted"), 3)
+
+    return model, train_accuracy, test_accuracy, duration
+
+
+def train_model(model, x_train, y_train, x_test, y_test):
+    t0 = time.time()
+
+    model.fit(x_train, y_train)
+    duration = time.time() - t0
+    y_train_pred = model.predict(x_train)
+    y_test_pred = model.predict(x_test)
+
+    train_accuracy = np.round(accuracy_score(y_train, y_train_pred), 3)
+    train_f1 = np.round(f1_score(y_train, y_train_pred, average="weighted"), 3)
+
+    test_accuracy = np.round(accuracy_score(y_test, y_test_pred), 3)
+    test_f1 = np.round(f1_score(y_test, y_test_pred, average="weighted"), 3)
+
+    return model, train_accuracy, train_f1, test_accuracy, test_f1, duration
+
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+
+def get_model_tips(model_type):
+    model_tips = model_infos[model_type]
+    return model_tips
+
+
+def get_model_url(model_type):
+    model_url = model_urls[model_type]
+    text = f"**Link to scikit-learn official documentation [here]({model_url}) 💻 **"
+    return text
+
+
+def add_polynomial_features(x_train, x_test, degree):
+    for d in range(2, degree + 1):
+        x_train = np.concatenate(
+            (
+                x_train,
+                x_train[:, 0].reshape(-1, 1) ** d,
+                x_train[:, 1].reshape(-1, 1) ** d,
+            ),
+            axis=1,
+        )
+        x_test = np.concatenate(
+            (
+                x_test,
+                x_test[:, 0].reshape(-1, 1) ** d,
+                x_test[:, 1].reshape(-1, 1) ** d,
+            ),
+            axis=1,
+        )
+    return x_train, x_test
---
+++ utils/ui.py
@@ -0,0 +1,217 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+from models.NaiveBayes import nb_param_selector
+
+from models.NeuralNetwork import nn_param_selector
+
+from models.RandomForet import rf_param_selector
+
+from models.DecisionTree import dt_param_selector
+
+from models.LogisticRegression import lr_param_selector
+
+from models.KNearesNeighbors import knn_param_selector
+
+from models.SVC import svc_param_selector
+
+from models.GradientBoosting import gb_param_selector
+
+from models.utils import model_imports
+
+from utils.functions import img_to_bytes
+
+# current_data: pd.DataFrame = None
+
+def introduction():
+    st.title("** Machine Learning Models for HTP phenotype prediction **")
+    st.subheader(
+        """
+        Using this website you can play with machine learning models to predict phenotypes from indices and assess the accuracy of predictions
+        """
+    )
+
+    st.markdown(
+        """
+    - 🗂️ Choose/upload a dataset
+    - ⚙️  Pick a model and set its hyper-parameters
+    - 📉 Train it and check its performance metrics  on train and test data
+    - 🩺 Diagnose possible overfitting and experiment with other settings
+    -----
+    """
+    )
+
+
+def dataset_selector():
+    dataset_container = st.sidebar.beta_expander("Configure a dataset", True)
+    global current_data
+
+    with dataset_container:
+        #st.radio(
+        #    "Choose file",
+        #    options=[
+        #       "Upload",
+        #       "Select from"
+        #   ]
+        #)
+
+        uploaded_file = st.file_uploader("Upload CSV file", key='data_uploader')
+
+        if uploaded_file is not None:
+            current_data = pd.read_csv(uploaded_file)
+            n_samples = current_data.shape[0]
+            # # From  versions above 1 use session state
+            # if 'cur_data' not in st.session_state:
+            #    st.session_state.cur_data = current_data
+
+        st.write("#### Or, choose a pre-loaded dataset")
+        dataset = st.selectbox("Choose a dataset", ("moons", "circles", "blobs"))
+
+        n_samples = st.number_input(
+            "Subset to a Number of samples:",
+            min_value=50,
+            max_value=1000,
+            step=10,
+            value=300,
+        )
+
+        train_noise = st.slider(
+            "Set the noise (train data)",
+            min_value=0.01,
+            max_value=0.2,
+            step=0.005,
+            value=0.06,
+        )
+        test_noise = st.slider(
+            "Set the noise (test data)",
+            min_value=0.01,
+            max_value=1.0,
+            step=0.005,
+            value=train_noise,
+        )
+
+        if dataset == "blobs":
+            n_classes = st.number_input("centers", 2, 5, 2, 1)
+        else:
+            n_classes = None
+
+        if uploaded_file is not None:
+            # current_data = pd.read_csv(uploaded_file)
+            # n_samples = current_data.shape[0]
+            n_classes = None
+            dataset = "upload"
+
+    return dataset, n_samples, train_noise, test_noise, n_classes
+
+
+def model_selector():
+    model_training_container = st.sidebar.beta_expander("Train a model", True)
+    with model_training_container:
+        model_type = st.selectbox(
+            "Choose a model",
+            (
+                "Logistic Regression", # Linear
+                "Decision Tree", #
+                "Random Forest", #
+                "Gradient Boosting",  #
+                "Neural Network", #
+                "K Nearest Neighbors", #
+                "Gaussian Naive Bayes", # remove
+                "SVC", # SVR
+            ),
+        )
+
+        if model_type == "Logistic Regression":
+            model = lr_param_selector()
+
+        elif model_type == "Decision Tree":
+            model = dt_param_selector()
+
+        elif model_type == "Random Forest":
+            model = rf_param_selector()
+
+        elif model_type == "Neural Network":
+            model = nn_param_selector()
+
+        elif model_type == "K Nearest Neighbors":
+            model = knn_param_selector()
+
+        elif model_type == "Gaussian Naive Bayes":
+            model = nb_param_selector()
+
+        elif model_type == "SVC":
+            model = svc_param_selector()
+
+        elif model_type == "Gradient Boosting":
+            model = gb_param_selector()
+
+    return model_type, model
+
+
+def generate_snippet(
+    model, model_type, n_samples, train_noise, test_noise, dataset, degree
+):
+    train_noise = np.round(train_noise, 3)
+    test_noise = np.round(test_noise, 3)
+
+    model_text_rep = repr(model)
+    model_import = model_imports[model_type]
+
+    if degree > 1:
+        feature_engineering = f"""
+    >>> for d in range(2, {degree+1}):
+    >>>     x_train = np.concatenate((x_train, x_train[:, 0] ** d, x_train[:, 1] ** d))
+    >>>     x_test= np.concatenate((x_test, x_test[:, 0] ** d, x_test[:, 1] ** d))
+    """
+
+    if dataset == "moons":
+        dataset_import = "from sklearn.datasets import make_moons"
+        train_data_def = (
+            f"x_train, y_train = make_moons(n_samples={n_samples}, noise={train_noise})"
+        )
+        test_data_def = f"x_test, y_test = make_moons(n_samples={n_samples // 2}, noise={test_noise})"
+
+    elif dataset == "circles":
+        dataset_import = "from sklearn.datasets import make_circles"
+        train_data_def = f"x_train, y_train = make_circles(n_samples={n_samples}, noise={train_noise})"
+        test_data_def = f"x_test, y_test = make_circles(n_samples={n_samples // 2}, noise={test_noise})"
+
+    elif dataset == "blobs":
+        dataset_import = "from sklearn.datasets import make_blobs"
+        train_data_def = f"x_train, y_train = make_blobs(n_samples={n_samples}, clusters=2, noise={train_noise* 47 + 0.57})"
+        test_data_def = f"x_test, y_test = make_blobs(n_samples={n_samples // 2}, clusters=2, noise={test_noise* 47 + 0.57})"
+
+    snippet = f"""
+    >>> {dataset_import}
+    >>> {model_import}
+    >>> from sklearn.metrics import accuracy_score, f1_score
+
+    >>> {train_data_def}
+    >>> {test_data_def}
+    {feature_engineering if degree > 1 else ''}    
+    >>> model = {model_text_rep}
+    >>> model.fit(x_train, y_train)
+    
+    >>> y_train_pred = model.predict(x_train)
+    >>> y_test_pred = model.predict(x_test)
+    >>> train_accuracy = accuracy_score(y_train, y_train_pred)
+    >>> test_accuracy = accuracy_score(y_test, y_test_pred)
+    """
+    return snippet
+
+
+def polynomial_degree_selector():
+    return st.sidebar.number_input("Highest polynomial degree", 1, 10, 1, 1)
+
+
+def footer():
+    st.sidebar.markdown("---")
+    st.sidebar.markdown(
+        """
+        [<img src='data:image/png;base64,{}' class='img-fluid' width=25 height=25>](https://github.com/ahmedbesbes/playground) <small> Based on Playground 0.1.0 </small>""".format(
+            img_to_bytes("./images/github.png")
+        ),
+        unsafe_allow_html=True,
+    )
